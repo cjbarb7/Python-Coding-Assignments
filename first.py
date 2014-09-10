11b03c3d0c28efe67f17d040557bb88f61b7ce91
CJ <cjbarb7@gmail.com>
2014-09-09 23:47:10 -0500

    adding the first assignment

---
+++ first.py
@@ -0,0 +1,12 @@
+##first assignment
+##to add
+##declare your variables
+firstVariable = 5
+
+secondVariable = 10
+
+##adding the two variables
+thirdVariable = firstVariable + secondVariable
+
+##print to the screen
+print thirdVariable
